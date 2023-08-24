0f58c6bfb516b4a5e70465bf68691c62bba30264
sirsa4 <yaya.abbdi@gmail.com>
2023-08-24 09:11:32 +0200

    oblig 1 start

---
+++ oppgave_0.js
@@ -0,0 +1,22 @@
+// Oppgave 1
+//get relevant html elements first
+const removeText = document.querySelector("#remove");
+const removeBtn = document.querySelector("#remove-btn");
+
+//function that removes the text
+const removeFunction = ()=>{
+    removeText.innerHTML = "";
+}
+
+//that was it
+//event listener which triggers when remove button is clicked on
+removeBtn.addEventListener("click",removeFunction); 
+
+// Oppgave 2
+// Oppgave 3
+// Oppgave 4
+const myList = ["item one", "item two", "item three"];
+// Oppgave 5
+// Oppgave 6
+// Oppgave 7
+// Oppgave 8
