b502df767a4ec2d16a21dd6cc19bfe41ecec4b27
sirsa4 <yaya.abbdi@gmail.com>
2023-08-24 09:34:17 +0200

    oppgave 0: oppgave 2 and 3 done

--- oppgave_0.js
+++ oppgave_0.js
@@ -8,13 +8,35 @@ const removeFunction = ()=>{
     removeText.innerHTML = "";
 }
 
-//that was it
-//test git
 //event listener which triggers when remove button is clicked on
 removeBtn.addEventListener("click",removeFunction); 
 
 // Oppgave 2
+//Here we also get relevant html elements we need to manipulate
+const changeText = document.querySelector("#change");
+const changeBtn = document.querySelector("#change-btn");
+
+//function which handles changing of text
+const changeTextFunction = ()=>{
+    changeText.textContent = "Text is now changed to something else";
+} 
+
+//event listener which triggers when change button is clicked on and runs the changeTextFunction which in end changes the text
+changeBtn.addEventListener("click",changeTextFunction);
+
 // Oppgave 3
+//Here we are also getting the releveant html elements first.
+const textInput = document.querySelector("#input");
+const textParagraph = document.querySelector("#input-text");
+
+//function which handles getting text from user input
+const HandleInput = (e)=>{
+    //console.log(e.target.value);
+    textParagraph.innerHTML = e.target.value;
+}
+
+//event listner which gets user input from keyboard
+textInput.addEventListener("keypress",HandleInput);
 // Oppgave 4
 const myList = ["item one", "item two", "item three"];
 // Oppgave 5
