2ff1bb68106d3022149dba126d27e20d65693bcc
sirsa4 <yaya.abbdi@gmail.com>
2023-08-25 12:11:00 +0200

    Oppgave 1 done

--- oppgave1.js
+++ oppgave1.js
@@ -9,9 +9,30 @@ const longestWord = () => {
   //source for split: https://fullstækk.no/courses/intro-javascript/07-javascript-dom-demo
   const words = text.split(" ");
   //console.log(words);
-  
+
   // TODO: Velg første ord så du har noe å sammenlikne med
+  //This stores the first world from the array. 
+  //This will be compared to next word in array
+  //if next word is longer, then this variable will store the longest word instead and keep doing this untill end of array
+  let firstWord = words[0];
   // TODO: Gå igjennom alle ordene og oppdater hvis nytt ord er lengre
+  //loop is running up length - 1. This is avoid undefined error which i was getting on return
+  for (let i = 0; i < words.length - 1; i++) {
+
+  /*
+  We are compared firstWord to the next word in the array.
+  if next word has lower length than the one before, then firstWord becomes the next word which has bigger length.
+  Loop is running only once, so word with biggest length will stay being firstWord untill untill the end.
+  */       
+  if(firstWord.length < words[i + 1].length){
+    firstWord = words[i + 1]; 
+    
+
+    }
+    
+    
+  }
+  return firstWord;
 };
 
 console.log(longestWord());
