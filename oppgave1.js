9fd6a88e699d85437df118dd76b38754e4dd5139
sirsa4 <yaya.abbdi@gmail.com>
2023-08-25 11:31:46 +0200

    words turned to array with split()

---
+++ oppgave1.js
@@ -0,0 +1,17 @@
+
+
+// Statisk tekst som brukes til å søke i
+const text =
+  "Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.";
+
+const longestWord = () => {
+  // TODO: Gjør om text til array eks. ['Baby', 'cliche']
+  //source for split: https://fullstækk.no/courses/intro-javascript/07-javascript-dom-demo
+  const words = text.split(" ");
+  //console.log(words);
+  
+  // TODO: Velg første ord så du har noe å sammenlikne med
+  // TODO: Gå igjennom alle ordene og oppdater hvis nytt ord er lengre
+};
+
+console.log(longestWord());
